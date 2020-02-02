fcf9b991e1b45641656312077fc257a70138b2d2
Andreas Aronsson <BOLL@7708.net>
2020-02-02 22:14:26 +0100

    Initial commit It works, but trying to replace the SteamVR binding fails, as does submitting the shot to Steam.

--- SuperScreenShotterVR/MainWindow.xaml.cs
+++ SuperScreenShotterVR/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BOLL7708;
+using Valve;
 
 namespace SuperScreenShotterVR
 {
@@ -20,9 +24,57 @@ namespace SuperScreenShotterVR
     /// </summary>
     public partial class MainWindow : Window
     {
+        private EasyOpenVRSingleton ovr = EasyOpenVRSingleton.Instance;
+        private bool initComplete = false;
         public MainWindow()
         {
             InitializeComponent();
+            var workerThread = new Thread(WorkerThread);
+            workerThread.Start();
+        }
+
+        private void WorkerThread()
+        {
+            Thread.CurrentThread.IsBackground = true;
+            while(true)
+            {
+                Thread.Sleep(10); // 100 Hz
+                if(!ovr.IsInitialized())
+                {
+                    ovr.Init();
+                    Thread.Sleep(1000);
+                } else
+                {
+                    if(!initComplete)
+                    {
+                        ovr.LoadAppManifest("./app.vrmanifest");
+                        ovr.LoadActionManifest("./actions.json");
+                        ovr.RegisterActionSet("/actions/default");
+                        ovr.RegisterDigitalAction(
+                            "/actions/default/in/take_screenshot", 
+                            (state) => { if(state) TakeScreenshot(); }
+                        );
+                        initComplete = true;
+                        Debug.WriteLine("Init complete.");
+                    } else
+                    {
+                        ovr.UpdateActionStates();
+                    }
+                }
+            }
+        }
+
+        private float originalScale = 1;
+
+        private void TakeScreenshot()
+        {
+            Debug.WriteLine("Taking screenshot!");
+            originalScale = ovr.GetRenderTargetForCurrentApp();
+            ovr.SetRenderScaleForCurrentApp(5f); // Clamped to 500%
+            Thread.Sleep(100); // Needs at least 50ms to change render scale before taking screenshot
+            ovr.TakeScreenshot();
+            ovr.SetRenderScaleForCurrentApp(originalScale);
+            Debug.WriteLine($"Screenshot taken! Original scale: {originalScale}");
         }
     }
 }
